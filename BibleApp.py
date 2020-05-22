3a376292b9e9a39f102b8865ba59916f45f37b88
pianocomposer321 <tmarklebw88@gmail.com>
2020-05-22 11:40:59 -0500

    Initial commit

---
+++ BibleApp.py
@@ -0,0 +1,14 @@
+import re
+
+class Verse():
+    def __init__(self, reference):
+        pass
+
+    def get_verse(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
