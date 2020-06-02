adde037900a71795fa17df40f088aca2cf76ae20
pianocomposer321 <tmarklebw88@gmail.com>
2020-06-02 17:59:35 -0500

    Added/fixed book numbering and verse ranges

--- BibleApp.py
+++ BibleApp.py
@@ -1,8 +1,19 @@
 import re
 import json
 
+
 class Verse:
     def __init__(self, reference=None):
+        self.kjv_bible = {}
+        self.reference = ''
+        self.book = ''
+        self.chapter = ''
+        self.verse = ''
+        self.verse_ext = ''
+        self.verses = []
+        self.passages = {}
+
+
         if reference is not None:
             self.reference = reference
             self.get_verse(reference=reference)
@@ -10,9 +21,7 @@ class Verse:
         self.open_data_file()
 
     def open_data_file(self):
-        with open("kjv.json", 'r+') as file:
-            # for book in json.load(file)["books"]:
-                                
+        with open("replace_I.json", 'r+') as file:
             self.kjv_bible = json.load(file)
 
     def get_verse(self, reference=None):
@@ -22,14 +31,8 @@ class Verse:
         BOOK_RE = re.compile(r"^\d*[a-zA-Z ]+")
         REF_RE = re.compile(r"\d{1,3}:\d{1,3}")
         VERSE_EXT_RE = re.compile(r"-(\d{1,3})")
-        REPLACE_ROMAN_RE = re.compile(r"^[1-3]")
 
         self.book = BOOK_RE.search(reference).group(0).strip()  # book = match of BOOK_RE minus trailing whitespace
-        # nr_of_Is = int(REPLACE_ROMAN_RE.search(self.book).group(0))
-        nr_of_Is = REPLACE_ROMAN_RE.search(self.book)
-        if nr_of_Is:
-            nr_of_Is = int(nr_of_Is.group(0))
-            self.book = REPLACE_ROMAN_RE.sub("I" * nr_of_Is, self.book)
 
         ref = REF_RE.search(reference).group(0)  # ref = match of REF_RE
         self.chapter, self.verse = ref.split(':')  # split chapter from verse
@@ -39,17 +42,8 @@ class Verse:
             self.verse_ext = self.verse_ext.group(1).strip()
             self.verse_ext = int(self.verse_ext)
 
-        if self.verse_ext:
-            print(f"The verse has an extension: {self.verse_ext}")
-            print()
-
-
-        # print(self.book)
-        # print(self.chapter)
-        # print(self.verse)
-
     def get_verse_text(self):
-        verses = []
+        # verses = []
         for book in self.kjv_bible['books']:
             if book["name"] == self.book:
                 for chapter in book["chapters"]:
@@ -61,20 +55,14 @@ class Verse:
                                     return
                             else:
                                 if verse["verse"] >= self.verse and verse["verse"] <= self.verse_ext:
-                                    verses.append(verse["text"])
-        if self.verse_ext:
-            for verse in verses:
-                print(verse)
-            print()
-        
+                                    self.verses.append(verse["text"])
+                                elif verse['verse'] > self.verse_ext:
+                                    for verse in self.verses:
+                                        print(verse)
+                                    print()
+                                    return
 
-        # for book in self.kjv_bible['books']:
-        #     if book["name"] == 'Genesis':
-        #         print("Found: Genesis")
-        #         return
-        #     else:
-        #         print("Not found: Genesis")
-        #         return
+
 
 def main():
     ref = input("Enter a reference: ")
